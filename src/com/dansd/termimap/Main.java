c12d03479cbe6c0f151f533301af0e51facf4c0d
Francisco Dans <fdansv@gmail.com>
2014-06-21 22:41:30 +0100

    adds exit call with Q

--- src/com/dansd/termimap/Main.java
+++ src/com/dansd/termimap/Main.java
@@ -26,7 +26,7 @@ public class Main {
                     newMode=true;
                 }
                 else if(input.equals("Q")){
-                    break;
+                    System.exit(0);
                 }
             }
 
